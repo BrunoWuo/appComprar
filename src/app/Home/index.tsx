dadfe026931015985d1ecae4e8059eb19591b61d
Bruno Wuo <brunocw@bol.com.br>
2025-12-06 18:44:45 -0300

    Componente button

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,15 @@
+import { Image, View } from "react-native"
+import { Button } from "@/components/Button"
+
+import { styles } from "./styles"
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
+
+      <Button/>
+
+    </View>
+  )
+}
