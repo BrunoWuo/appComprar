6e69cf971a242c6a6b6ef3a89d865383ee9431a6
Bruno Wuo <brunocw@bol.com.br>
2025-12-06 19:13:36 -0300

    componente input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,13 +1,17 @@
 import { Image, View } from "react-native"
+
+import { Input } from "@/components/Input"
 import { Button } from "@/components/Button"
 
 import { styles } from "./styles"
+
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
 
+      <Input placeholder="O que você precisa comprar?"/>
       <Button title="Entrar" onPress={()=>console.log("salvar")}/>
 
     </View>
